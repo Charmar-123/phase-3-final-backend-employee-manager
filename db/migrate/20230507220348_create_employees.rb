d9cb2785ec0ea5566ebd28b1e512d13f1868b9c7
Charbel Maroun <charbel-maroun@hotmail.com>
2023-05-07 18:06:41 -0400

    create the tables for employees and tasks

--- db/migrate/20230507220348_create_employees.rb
+++ db/migrate/20230507220348_create_employees.rb
@@ -1,4 +1,8 @@
 class CreateEmployees < ActiveRecord::Migration[6.1]
   def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :position
+    end
   end
 end
--- db/migrate/20230507220409_create_tasks.rb
+++ db/migrate/20230507220409_create_tasks.rb
@@ -1,4 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
+    create_table :tasks do |t|
+      t.string :description
+      t.boolean :status
+      t.integer :employee_id
+    end
   end
 end
