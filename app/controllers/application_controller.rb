8981e2a4f4c9dcb2f596b95a4ff4dbab8b6a14c5
Charbel Maroun <charbel-maroun@hotmail.com>
2023-05-10 18:26:11 -0400

    add post route to application controller for employee

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+
   get "/employees" do
     employees = Employee.all
     employees.to_json include: [:tasks]
   end
+
+  post "/employees" do
+    employee = Employee.create(params)
+    employee.to_json
 
   post "/tasks" do
     task = Task.create(params)
